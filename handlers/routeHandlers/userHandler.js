4da5e3a6f1736d066b62f7b74335f53ddd18b502
Evan <evan@Evans-MacBook-Air.local>
2021-09-15 19:18:23 +0600

    User Update And Delete

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -33,7 +33,7 @@ handler._users.post = (requestProperties,callback) => {
   && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password :false
 
   const tosAgreement = typeof(requestProperties.body.tosAgreement) === 'boolean' ? requestProperties.body.tosAgreement :false
-  console.log(tosAgreement,password,firstName,lastName);
+  
   if(firstName && lastName && phone && password && tosAgreement){
     data.read('users',phone,(err,res)=>{
       
@@ -93,8 +93,98 @@ handler._users.get = (requestProperties,callback) => {
     })
   }
 }
-handler._users.put = (requestProperties,callback) => {}
-handler._users.delete = (requestProperties,callback) => {}
+
+handler._users.put = (requestProperties,callback) => {
+  const phone = typeof(requestProperties.body.phone) === 'string' 
+  && requestProperties.body.phone.trim().length === 11 
+  ? requestProperties.body.phone 
+  :false
+
+  const firstName = typeof(requestProperties.body.firstName) === 'string' 
+  && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName :false
+
+  const lastName = typeof(requestProperties.body.lastName) === 'string' 
+  && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName :false
+
+  const password = typeof(requestProperties.body.password) === 'string' 
+  && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password :false
+
+  if (phone) {
+    if (firstName || lastName || password) {
+      data.read('users',phone,(err,res)=>{
+        let userData = {...parseJSON(res)}
+        if(!err && userData){
+          if (firstName) {
+            userData.firstName = firstName
+          }
+
+          if (lastName) {
+            userData.lastName = lastName
+          }
+
+          if (password) {
+            userData.password = hash(password)
+          }
+
+          data.update('users',phone,userData,(err)=>{
+            if(!err) {
+              delete userData.password
+              callback(200,{
+                message:'User Updated Sucessfully',
+                data:userData
+              })
+            }else{
+              callback(500,{
+                error:'Couldn\'t updated'
+              })
+            }
+          })
+        }else{
+          callback(404,{
+            error:"User Not Found"
+          })
+        }
+      })
+    }
+  }else{
+    callback(400,{
+      error:'Invalid Phone Number'
+    })
+  }
+
+}
+handler._users.delete = (requestProperties,callback) => {
+  const phone = typeof(requestProperties.queryStringObject.phone) === 'string' 
+  && requestProperties.queryStringObject.phone.trim().length === 11 
+  ? requestProperties.queryStringObject.phone 
+  :false
+
+  if (phone) {
+    data.read('users',phone,(err,res)=>{
+      if (!err && res) {
+        data.delete('users',phone,(err)=>{
+          if (!err) {
+            callback(200,{
+              message:'User Deleted Sucessfully'
+            })
+          }else{
+            callback(500,{
+              error:'Couldn\'t Delete'
+            })
+          }
+        })
+      }else{
+        callback(500,{
+          error:'Couldn\'t Find The User'
+        })
+      }
+    })
+  }else{
+    callback(401,{
+      error:"Invalid Number"
+    })
+  }
+}
 
 
 module.exports = handlers
