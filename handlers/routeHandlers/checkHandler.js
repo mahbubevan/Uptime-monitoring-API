34ef8ded230cf9e9fa80d2f9e906191b7db0147c
mahbubevan <evanpulse92@gmail.com>
2021-09-18 23:11:36 +0600

    Url Check CRUD

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -56,12 +56,12 @@ handler._check.post = (requestProperties,callback) => {
             if (!err && userData) {
               verifyToken(token,userPhone,(res)=>{
                 if (res) {
-                  let userObject = parseJSON(userData)
+                  let userObject = {...parseJSON(userData)}
                   let userChecks = typeof userObject.checks === 'object'
                   && userObject.checks instanceof Array 
                   ? userObject.checks : []
                   
-                  if (userChecks < env.maxCheck) {
+                  if (userChecks.length < env.maxCheck) {
                     let checkId = createRandomString(20)
                     let checkObject = {
                       id:checkId,
@@ -128,16 +128,210 @@ handler._check.post = (requestProperties,callback) => {
     }
 }
 
-
-
 handler._check.get = (requestProperties,callback) => {
-  console.log("Hello");
-  callback(200,{
-    msg:"Good"
-  })
+    const id = typeof requestProperties.queryStringObject.id === 'string' 
+            && requestProperties.queryStringObject.id.length === 20
+            ? requestProperties.queryStringObject.id : false 
+    
+    if (id) {
+      data.read('check',id,(err,res)=>{
+        if(err){
+          callback(404,{
+            error:'Not Found'
+          })
+        }else{
+          let token = typeof requestProperties.headerObject.bearer === 'string'
+          ? requestProperties.headerObject.bearer :false
+          let checkObject = {... parseJSON(res)}
+          let phone = checkObject.userPhone
+          verifyToken(token,phone,(tokenIsValid)=>{
+            if(!tokenIsValid){
+              callback(403,{
+                error:'Unauthorized'
+              })
+            }else{
+              callback(200,checkObject)
+            }
+          })
+        }
+      })
+    }else{
+      callback(404,{
+        error:'Data Not Found'
+      })
+    }
 }
-handler._check.put = (requestProperties,callback) => {}
-handler._check.delete = (requestProperties,callback) => {}
+
+handler._check.put = (requestProperties,callback) => {
+  const id = typeof requestProperties.body.id === 'string' 
+            ? requestProperties.body.id : false
+  
+            console.log(id);
+  //validate inputs 
+  let protocol = typeof requestProperties.body.protocol === 'string'
+    && ['http','https'].indexOf(requestProperties.body.protocol) > -1 
+    ? requestProperties.body.protocol :false 
+  
+    let url = typeof requestProperties.body.url === 'string' 
+      && requestProperties.body.url.trim().length > 0 
+      ? requestProperties.body.url :false 
+
+    let method = typeof requestProperties.body.method === 'string' 
+    && ['get','post','put','delete'].indexOf(requestProperties.body.method) > -1 
+    ? requestProperties.body.method :false 
+
+    let successCodes = typeof requestProperties.body.successCodes === 'object'
+    && requestProperties.body.successCodes instanceof Array 
+    ? requestProperties.body.successCodes 
+    :false
+
+    let timeoutSeconds = typeof requestProperties.body.timeoutSeconds === 'number' 
+      && requestProperties.body.timeoutSeconds % 1 === 0
+      && requestProperties.body.timeoutSeconds >= 1
+      && requestProperties.body.timeoutSeconds <= 5
+      ? requestProperties.body.timeoutSeconds :false
+  
+    if (!id) {
+      callback(400,{
+        error:'Id is required'
+      })
+    }
+    else{
+      if (protocol || url || method || successCodes || timeoutSeconds) {
+        data.read('check',id,(err,checkData)=>{
+          if (err && !checkData) {
+            callback(500,{
+              error:'Couldn\'t find the data'
+            })
+          }else{
+            let checkObject = {...parseJSON(checkData)}
+            let token = typeof requestProperties.headerObject.bearer === 'string'
+            ? requestProperties.headerObject.bearer :false
+
+            verifyToken(token,checkObject.userPhone,(tokenIsValid)=>{
+              if (!tokenIsValid) {
+                callback(403,{
+                  error:'Unauthorized Token'
+                })
+              }else{
+                if (protocol) {
+                  checkObject.protocol = protocol
+                }
+
+                if (url) {
+                  checkObject.url = url 
+                }
+                
+                if (method) {
+                  checkObject.method = method
+                }
+
+                if (successCodes) {
+                  checkObject.successCodes = successCodes
+                }
+
+                if (timeoutSeconds) {
+                  checkObject.timeoutSeconds = timeoutSeconds
+                }
+
+                data.update('check',id,checkObject,(err)=>{
+                  if (err) {
+                    callback(500,{
+                      error:'Couldn\'t Updated'
+                    })
+                  }else{
+                    callback(200,{
+                      msg:'Updated Successfully',
+                      data:checkObject
+                    })
+                  }
+                })
+                
+              }
+            })
+
+          }
+        })
+      }else{
+        callback(400,{
+          error:"No filed to update"
+        })
+      }
+    }
+}
+
+
+handler._check.delete = (requestProperties,callback) => {
+  const id = typeof requestProperties.queryStringObject.id === 'string' 
+            ? requestProperties.queryStringObject.id : false
+  
+  if (!id) {
+    callback(400,{
+      error:'Id Required'
+    })
+  }else{
+    data.read('check',id,(err,checkData)=>{
+      if (err && !checkData) {
+        callback(400,{
+          error:'Couldn\'t Find The Data'
+        })
+      }else{
+        let phone = parseJSON(checkData).userPhone 
+        let token = typeof requestProperties.headerObject.bearer === 'string'
+        ? requestProperties.headerObject.bearer :false 
+
+        verifyToken(token,phone,(isValidToken)=>{
+          if (!isValidToken) {
+            callback(403,{
+              error:'Unauthorized Action'
+            })
+          }else{
+            data.delete('check',id,(err)=>{
+              if (err) {
+                callback(500,{
+                  error:'Server Side Problem'
+                })
+              }else{
+                data.read('users',phone,(err,userData)=>{
+                  if (err) {
+                    callback(500,{error:'Couldn\'t Find The Data'})
+                  }else{
+                    let userObject = parseJSON(userData)
+                    let userChecks =  typeof userObject.checks === 'object' 
+                    && userObject.checks instanceof Array 
+                    ? userObject.checks 
+                    : []
+
+                    let checkPos = userChecks.indexOf(id)
+                    if (checkPos > -1) {
+                      userChecks.splice(checkPos,1)
+                      userObject.checks = userChecks
+                      data.update('users',phone,userObject,(err)=>{
+                        if (err) {
+                          callback(500,{error:'Server Error'})
+                        }else{
+                          callback(200,{
+                            msg:'Updated Successfully',
+                            data:userObject
+                          })
+                        }
+                      })
+                    }else{
+                      callback(400,{
+                        error:'The check id is not available'
+                      })
+                    }
+                  }
+                })
+              }
+            })
+          }
+        })
+
+      }
+    })
+  }
+}
 
 
 module.exports = handlers
--- index.js
+++ index.js
@@ -3,27 +3,8 @@ const http = require('http')
 const {handleReqRes} = require('./helpers/handleReqRes')
 const env = require('./helpers/env')
 
-const data = require('./lib/data')
-
 // App object - module scaffolding
 const app = {}
-
-//Testing File System
-
-// data.create('test','newFile',{'name':"Evan",'age':27},(err,result)=>{
-//   console.log(err,result);
-// })
-// data.read('test','newFile',(err,data)=>{
-//   console.log(err,data);
-// })
-
-// data.update('test','newFile',{'name':'Safa','age':7},(err)=>{
-//   console.log(err);
-// })
-
-// data.delete('test','newFile',(err,res)=>{
-//   console.log(err,res);
-// })
 
 // create server 
 app.createServer = () => {
